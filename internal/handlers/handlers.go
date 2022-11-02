7b1c59a75f016fe96759c34ad07257739b55855a
rudsonalves <rudsonalves67@gmail.com>
2022-11-02 06:57:32 -0300

    Rename directory nema pkg to internal

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,109 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/rudsonalves/bookings/internal/config"
+	"github.com/rudsonalves/bookings/internal/models"
+	"github.com/rudsonalves/bookings/internal/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo create a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m Repository) Home(write http.ResponseWriter, request *http.Request) {
+	remoteIP := request.RemoteAddr
+	m.App.Session.Put(request.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(write, request, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m Repository) About(write http.ResponseWriter, request *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(request.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send the data to the template
+	render.RenderTemplate(write, request, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
+
+// Reservation renders the make a reservation page and displays form
+func (m Repository) Reservation(write http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(write, request, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Generals renders the room page
+func (m Repository) Generals(write http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(write, request, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors renders the room page
+func (m Repository) Majors(write http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(write, request, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// Contact renders the contact page and displays form
+func (m Repository) Contact(write http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(write, request, "contact.page.tmpl", &models.TemplateData{})
+}
+
+// Availability renders the search availability page and displays form
+func (m Repository) Availability(write http.ResponseWriter, request *http.Request) {
+	render.RenderTemplate(write, request, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// PostAvailability renders the search availability page and displays form
+func (m Repository) PostAvailability(write http.ResponseWriter, request *http.Request) {
+	start := request.Form.Get("start")
+	end := request.Form.Get("end")
+
+	write.Write([]byte(fmt.Sprintf("Posted to search availability from %s to %s", start, end)))
+}
+
+type jsonResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m Repository) AvailabilityJSON(write http.ResponseWriter, request *http.Request) {
+	response := jsonResponse{
+		Ok:      false,
+		Message: "Not available!",
+	}
+
+	out, err := json.MarshalIndent(response, "", "    ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	write.Header().Set("Content-Type", "application/json")
+	write.Write(out)
+}
---
+++ internal/render/render.go
@@ -0,0 +1,93 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/justinas/nosurf"
+	"github.com/rudsonalves/bookings/internal/config"
+	"github.com/rudsonalves/bookings/internal/models"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+// AddDefaultData adds data for all templates
+func AddDefaultData(td *models.TemplateData, request *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(request)
+	return td
+}
+
+// RenderTemplate renders a template
+func RenderTemplate(write http.ResponseWriter, request *http.Request, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td, request)
+
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(write)
+	if err != nil {
+		fmt.Println("error writing template to browser", err)
+	}
+
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
